rotation transform test and fixes

pyapcwt/transforms2d.py
@@ -10,7 +10,7 @@ NumpyArray = npt.NDArray[np.float64 | np.float32]
 class ITransform2D(ABC):
 
     def __call__(self, vec2d: NumpyArray) -> NumpyArray:
-        assert vec2d.ndim == 2 or vec2d.ndim == 1
+        assert vec2d.ndim == 2
         assert vec2d.shape[0] == 2
 
         return self.apply(vec2d.reshape((2, -1)))
@@ -125,7 +125,6 @@ class Translation2D(ITransform2D):
         assert vec2d.shape[0] == 2
         assert vec2d.ndim == 2
         return vec2d + self.__translation
-
 
 
 class DummyTransform2D(ITransform2D):

tests/test_transforms2d.py
@@ -3,10 +3,12 @@ import numpy as np
 
 import pyapcwt.transforms2d as transforms2d
 
+EPSILON_FOR_EXACT_FP64 = 10 * np.finfo(np.float64).eps
+
 
 class TestTranslation2d(unittest.TestCase):
     def test_neutral_translation(self):
-        vec2d = np.array([1, 4])
+        vec2d = np.array([1, 4]).reshape((2, 1))
         zero_translation = transforms2d.Translation2D(np.array([0, 0]))
         neutral_result = zero_translation(vec2d)
         self.assertTrue((vec2d.reshape(2, -1) == neutral_result).all())
@@ -42,7 +44,7 @@ class TestTranslation2d(unittest.TestCase):
         self.assertTrue((i2t == vecs2d).all())
 
     def test_apply(self):
-        vec2d = np.array([2.5, -1.125]).reshape((2,1))
+        vec2d = np.array([2.5, -1.125]).reshape((2, 1))
         vec2d_vectorized = np.array([[2.5, -1.125], [1.125, 2], [-5, 2]])
 
         t = transforms2d.Translation2D(np.array([2.5, -1.125]))
@@ -55,6 +57,83 @@ class TestTranslation2d(unittest.TestCase):
 
         self.assertTrue((vec2d_expected == vec2d_res).all())
         self.assertTrue((vec2d_vectorized_expected == vec2d_vectorized_res).all())
+
+
+class TestRotation2D(unittest.TestCase):
+    def test_neutral_rotation(self):
+        vec2d = np.array([1, 4]).reshape((2, 1))
+        zero_rotation = transforms2d.Rotation2D(0)
+        neutral_result = zero_rotation(vec2d)
+        self.assertTrue((vec2d == neutral_result).all())
+
+        zero_rotation = transforms2d.Rotation2D(np.pi * 4)
+        neutral_result = zero_rotation(vec2d)
+        self.assertTrue(np.allclose(vec2d, neutral_result, EPSILON_FOR_EXACT_FP64, EPSILON_FOR_EXACT_FP64))
+
+    def test_compose(self):
+        vec2d = np.array([1, 1]).reshape(2, -1)
+
+        t1 = transforms2d.Rotation2D(np.pi / 3)
+        t2 = transforms2d.Rotation2D(-np.pi / 3)
+
+        t = t1 * t2
+        neutral_result = t(vec2d)
+        self.assertTrue((vec2d == neutral_result).all())
+
+        t2 = transforms2d.Rotation2D(2 * np.pi / 3)
+
+        vl = (t1 * t2).apply(vec2d)
+        vr = (t2 * t1).apply(vec2d)
+
+        v_expected = np.array([-1, -1]).reshape(2, -1)
+
+        self.assertTrue((vl == vr).all())
+        self.assertTrue(np.allclose(v_expected, vl, EPSILON_FOR_EXACT_FP64, EPSILON_FOR_EXACT_FP64))
+
+    def test_inv(self):
+        vecs2d = np.array([[2.5, -1.125], [1.125, 2.5, ]])
+
+        t = transforms2d.Rotation2D(0.3351)
+        t_inv = t.inv()
+
+        i2t = (t_inv * t).apply(vecs2d)
+        t2i = (t * t_inv).apply(vecs2d)
+        self.assertTrue((i2t == t2i).all())
+        self.assertTrue(np.allclose(i2t, vecs2d, EPSILON_FOR_EXACT_FP64, EPSILON_FOR_EXACT_FP64))
+
+    def test_apply(self):
+        vec2d = np.array([2.5, -1.125]).reshape((2, 1))
+        vec2d_vectorized = np.array([[2.5, -1.125], [1.125, 2.5], [-5, 2]]).transpose()
+
+        angle = 3.5
+        r = transforms2d.Rotation2D(angle)
+
+        vec2d_res = r.apply(vec2d)
+        vec2d_vectorized_res = r.apply(vec2d_vectorized)
+
+        angle_res = np.arccos(np.vdot(vec2d / np.linalg.norm(vec2d), vec2d_res / np.linalg.norm(vec2d_res)))
+        cross_z = vec2d[0] * vec2d_res[1] - vec2d[1] * vec2d_res[0]
+        # 3.5 > pi
+        self.assertTrue(cross_z < 0)
+        self.assertTrue(np.allclose(np.linalg.norm(vec2d), np.linalg.norm(vec2d_res), EPSILON_FOR_EXACT_FP64,
+                                    EPSILON_FOR_EXACT_FP64))
+        # 3.5 > pi => angle_res should be taken with
+        self.assertTrue(np.allclose(angle_res, 2 * np.pi - angle, EPSILON_FOR_EXACT_FP64, EPSILON_FOR_EXACT_FP64))
+
+        self.assertTrue(np.allclose(vec2d_res, vec2d_res[:, 0:1], EPSILON_FOR_EXACT_FP64, EPSILON_FOR_EXACT_FP64))
+        # keep orthogonality
+        self.assertTrue(np.allclose(
+            0,
+            np.vdot(vec2d_vectorized_res[:, 0:1], vec2d_vectorized_res[:, 1:2]),
+            EPSILON_FOR_EXACT_FP64,
+            EPSILON_FOR_EXACT_FP64))
+
+        # we compare 1-st vectors, orthogonality of fist & second and I believe it is enough to compare the whole norm
+        self.assertTrue(np.allclose(
+            np.linalg.norm(vec2d_vectorized),
+            np.linalg.norm(vec2d_vectorized_res),
+            EPSILON_FOR_EXACT_FP64,
+            EPSILON_FOR_EXACT_FP64))
 
 
 if __name__ == '__main__':